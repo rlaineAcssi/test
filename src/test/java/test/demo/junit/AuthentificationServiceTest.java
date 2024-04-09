73795469565512186b3884d62e8ee1b2f95a720b
Rémi LAINE <rlaine@acssi.fr>
2024-04-09 16:43:40 +0200

    fix AuthenticationServiceTest

--- src/test/java/test/demo/junit/AuthentificationServiceTest.java
+++ src/test/java/test/demo/junit/AuthentificationServiceTest.java
@@ -1,24 +1,14 @@
 package test.demo.junit;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import test.demo.security.AuthenticationService;
 
 @SpringBootTest
 public class AuthentificationServiceTest {
-
-	@Autowired
-	private AuthenticationService authentificationService;
-
-	@Test
-	void assertAutentificationNotNull() {
-		assertNotNull(authentificationService);
-	}
 
 	@Test
 	void assertExceptionWHenUsePublicConstructor() {

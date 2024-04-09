4f6d83503e2931108341953c834bc62e60237b96
Rémi LAINE <rlaine@acssi.fr>
2024-04-09 16:56:50 +0200

    fix AuthenticationServiceTest

--- src/test/java/test/demo/junit/AuthentificationServiceTest.java
+++ src/test/java/test/demo/junit/AuthentificationServiceTest.java
@@ -1,9 +1,12 @@
 package test.demo.junit;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.authentication.BadCredentialsException;
 
 import test.demo.security.AuthenticationService;
 
@@ -13,7 +16,27 @@ public class AuthentificationServiceTest {
 	@Test
 	void assertExceptionWHenUsePublicConstructor() {
 		assertThrows(IllegalStateException.class, () -> new AuthenticationService());
+	}
 
+	@Test
+	void getInvalidAuthentificationThrowError() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.addHeader("X-API-KEY", "toto");
+		assertThrows(BadCredentialsException.class, () -> AuthenticationService.getAuthentication(request));
+	}
+
+	@Test
+	void getInvalidHeaderThrowError() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.addHeader("toto", "no-database");
+		assertThrows(BadCredentialsException.class, () -> AuthenticationService.getAuthentication(request));
+	}
+
+	@Test
+	void assertIdentification() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.addHeader("X-API-KEY", "no-database");
+		assertNotNull(AuthenticationService.getAuthentication(request));
 	}
 
 }

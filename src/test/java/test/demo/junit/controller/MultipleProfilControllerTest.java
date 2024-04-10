24ef5b2abcb7d6cf935fafd0ff61ef126edce555
Rémi LAINE <rlaine@acssi.fr>
2024-04-10 11:04:08 +0200

    add first mockMvc with token

--- src/test/java/test/demo/junit/controller/MultipleProfilControllerTest.java
+++ src/test/java/test/demo/junit/controller/MultipleProfilControllerTest.java
@@ -1,9 +1,16 @@
 package test.demo.junit.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import net.minidev.json.JSONArray;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -11,5 +18,13 @@ public class MultipleProfilControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Test
+	void ensureMultipleProfilCreate() throws Exception {
+		JSONArray ja = new JSONArray();
+		ja.appendElement("test");
+		this.mockMvc.perform(post("/profil/create-multiple").header("X-API-KEY", "no-database")
+				.contentType(MediaType.APPLICATION_JSON).content(ja.toJSONString())).andExpect(status().isOk());
+	}
 
 }
